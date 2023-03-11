1d05153d2f50c8fb19d8727ff653e03677a0f05b
Nikita <nikita.kopeickin@yandex.ru>
2023-03-11 19:28:45 +0300

    costs of the company

---
+++ task_4.py
@@ -0,0 +1,15 @@
+income = int(input("Введите выручку фирмы: "))
+costs = int(input("Введите издержки фирмы: "))
+
+if income > costs:
+    print("Выручка больше издержек, фирма работает в прибыль")
+    result = income - costs
+    print(f"Прибыль фирмы: {result}")
+    profitability = result / income
+    print(f"Рентабельность выручки: {profitability}")
+    quantity_staffers = int(input("Введите численность сотрудников фирмы: "))
+    print(f"Прибыль фирмы в расчете на одного сотрудника "
+          f"= {result / quantity_staffers}")
+
+else:
+    print("Издержки больше выручки, фирма работает в убыток")
