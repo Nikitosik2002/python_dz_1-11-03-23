7639c580cd5fb50a8e4eaca364a63ed092dc9195
Nikita <nikita.kopeickin@yandex.ru>
2023-03-11 19:24:17 +0300

    first done

---
+++ task_1.py
@@ -0,0 +1,5 @@
+user_name = input("Введите ваше имя: ")
+user_password = input("Ведите ваш пароль: ")
+user_age = int(input("Введите ваш возраст: "))
+print(f"Ваши данные для входа в аккаунт: имя - {user_name}, "
+      f"пароль - {user_password}, возраст - {user_age}")
