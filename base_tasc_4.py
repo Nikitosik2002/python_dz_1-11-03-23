14cadc754ee9086ace40e5bf2c179db6c3a9fa93
Nikita <nikita.kopeickin@yandex.ru>
2023-03-19 21:41:11 +0300

    base_4 fixed pep8

--- base_tasc_4.py
+++ base_tasc_4.py
@@ -1,13 +1,13 @@
 def my_func(arg_1, arg_2, arg_3):
-   list_1 = [arg_1, arg_2, arg_3]
-   size = len(list_1)
-   for i in range((size-1)):
+    list_1 = [arg_1, arg_2, arg_3]
+    size = len(list_1)
+    for i in range((size-1)):
         max = i
         for j in range(i+1, size):
             if list_1[max] < list_1[j]:
                 max = j
         list_1[i], list_1[max] = list_1[max], list_1[i]
-   return list_1[0] + list_1[1]
+    return list_1[0] + list_1[1]
 
 
 arg_1 = int(input("Введите первое число:"))
