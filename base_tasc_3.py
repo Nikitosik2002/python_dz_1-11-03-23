866eeac22f5d2352ea3da0c2b5bd58549cb6c714
Nikita <nikita.kopeickin@yandex.ru>
2023-03-19 21:40:58 +0300

    base_3 fixed pep8

--- base_tasc_3.py
+++ base_tasc_3.py
@@ -2,6 +2,7 @@ def print_func(name, surname, year_of_birth, city, email, telephone_number):
     print(F"{name} {surname} {year_of_birth} года рождения, проживает в городе"
           F" {city} \nemail: {email}, телефон: {telephone_number}")
 
+
 name = input("Введите свое имя:")
 surname = input("Введите свою фамилию:")
 year_of_birth = int(input("Введите год своего рождения:"))
