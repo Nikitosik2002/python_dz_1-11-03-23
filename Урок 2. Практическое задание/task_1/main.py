af3841813d169fc5e377a4e14a7f8cc8a43a2598
Nikita <nikita.kopeickin@yandex.ru>
2023-04-07 12:15:04 +0300

    t_1_main, t_2_main, t_2_json, t_3_yaml, t_3_main

--- Урок 2. Практическое задание/task_1/main.py
+++ Урок 2. Практическое задание/task_1/main.py
@@ -1,42 +1,56 @@
-"""
-1. Задание на закрепление знаний по модулю CSV. Написать скрипт,
-осуществляющий выборку определенных данных из файлов info_1.txt, info_2.txt,
-info_3.txt и формирующий новый «отчетный» файл в формате CSV.
-
-Для этого:
-
-Создать функцию get_data(), в которой в цикле осуществляется перебор файлов
-с данными, их открытие и считывание данных. В этой функции из считанных данных
-необходимо с помощью регулярных выражений извлечь значения параметров
-«Изготовитель системы», «Название ОС», «Код продукта», «Тип системы».
-Значения каждого параметра поместить в соответствующий список. Должно
-получиться четыре списка — например, os_prod_list, os_name_list,
-os_code_list, os_type_list. В этой же функции создать главный список
-для хранения данных отчета — например, main_data — и поместить в него
-названия столбцов отчета в виде списка: «Изготовитель системы»,
-«Название ОС», «Код продукта», «Тип системы». Значения для этих
-столбцов также оформить в виде списка и поместить в файл main_data
-(также для каждого файла);
-
-Создать функцию write_to_csv(), в которую передавать ссылку на CSV-файл.
-В этой функции реализовать получение данных через вызов функции get_data(),
-а также сохранение подготовленных данных в соответствующий CSV-файл;
-
-Пример того, что должно получиться:
-
-Изготовитель системы,Название ОС,Код продукта,Тип системы
-
-1,LENOVO,Windows 7,00971-OEM-1982661-00231,x64-based
-
-2,ACER,Windows 10,00971-OEM-1982661-00231,x64-based
-
-3,DELL,Windows 8.1,00971-OEM-1982661-00231,x86-based
-
-Обязательно проверьте, что у вас получается примерно то же самое.
-
-ПРОШУ ВАС НЕ УДАЛЯТЬ СЛУЖЕБНЫЕ ФАЙЛЫ TXT И ИТОГОВЫЙ ФАЙЛ CSV!!!
-"""
+import re
+import csv
 
 
-os_prod_reg = re.compile(r'Изготовитель системы:\s*\S*')
-os_prod_list.append(os_prod_reg.findall(data)[0].split()[2])
+def get_data(file, number_f):
+    number_file = [number_f]
+    os_prod_list = []
+    os_name_list = []
+    os_code_list = []
+    os_type_list = []
+    main_data = [
+        ['Изготовитель системы', 'Название ОС', 'Код продукта', 'Тип системы']]
+    with open(file) as line:
+        all_lines = line.read()
+        os_prod_list.append(
+            re.compile(r'Изготовитель системы:\s*\S*').findall(all_lines)[
+                0].split()[2])
+        os_name_list.append(
+            re.compile(r'Название ОС:\s*\S*\s*\S*\s*').findall(all_lines)[
+                0].split()[3])
+        os_name_list.append(
+            re.compile(r'Название ОС:\s*\S*\s*\S*\s*\d*').findall(all_lines)[
+                0].split()[4])
+        os_code_list.append(
+            re.compile(r'Код продукта:\s*\S*').findall(all_lines)[0].split()[
+                2])
+        os_type_list.append(
+            re.compile(r'Тип системы:\s*\S*').findall(all_lines)[0].split()[2])
+
+        os_name_list[0: 2] = [' '.join(os_name_list[0: 2])]
+        main_data = [
+            *number_file,
+            *os_prod_list,
+            *os_name_list,
+            *os_code_list,
+            *os_type_list
+        ]
+        return main_data
+
+
+def write_to_csv(file, res):
+    with open(file, 'w') as f_n:
+        write = csv.writer(f_n, delimiter=",", lineterminator="\r")
+        write.writerow(["Изготовитель системы", "Название ОC", "Код продукта",
+                        "Тип системы"])
+        for line in res:
+            write.writerow(line)
+
+
+f_1 = get_data('info_1.txt', 1)
+f_2 = get_data('info_2.txt', 2)
+f_3 = get_data('info_3.txt', 3)
+
+result = (f_1, f_2, f_3)
+
+write_to_csv('new.csv', result)
--- Урок 2. Практическое задание/task_2/main.py
+++ Урок 2. Практическое задание/task_2/main.py
@@ -1,25 +1,22 @@
-"""
-2. Задание на закрепление знаний по модулю json. Есть файл orders
-в формате JSON с информацией о заказах. Написать скрипт, автоматизирующий
-его заполнение данными.
+import json
 
-Для этого:
-Создать функцию write_order_to_json(), в которую передается
-5 параметров — товар (item), количество (quantity), цена (price),
-покупатель (buyer), дата (date). Функция должна предусматривать запись
-данных в виде словаря в файл orders.json. При записи данных указать
-величину отступа в 4 пробельных символа;
-Проверить работу программы через вызов функции write_order_to_json()
-с передачей в нее значений каждого параметра.
 
-ПРОШУ ВАС НЕ УДАЛЯТЬ ИСХОДНЫЙ JSON-ФАЙЛ
-ПРИМЕР ТОГО, ЧТО ДОЛЖНО ПОЛУЧИТЬСЯ
+def write_order_to_json(it, quan, pr, buy, dat):
+    dict_in_json = {
+        "item": it,
+        "quantity": quan,
+        "price": pr,
+        "buyer": buy,
+        "date": dat
 
-{
-    "orders": []
-}
+    }
 
-вам нужно подгрузить JSON-объект
-и достучаться до списка, который и нужно пополнять
-а потом сохранять все в файл
-"""
+    with open('orders.json') as f_n:
+        obj = json.load(f_n)
+        obj['orders'].append(dict_in_json)
+
+    with open('orders.json', 'w') as f_w:
+        json.dump(obj, f_w, indent=4)
+
+
+write_order_to_json("json", "3", "400", "vasiliy", "asdf")
